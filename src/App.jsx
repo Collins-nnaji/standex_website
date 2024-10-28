ff37ed94ba24be4e1e333f568342a2bbb40346ed
Collins Nnaji <collinsnnaji1@gmail.com>
2024-10-28 21:49:54 +0000

    sss

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import Home from './components/Home'
-import TechElevate from './components/TechElevate'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import Home from './components/Home';
+import TechElevate from './components/TechElevate';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+
+const trackPageView = (url) => {
+  if (typeof window.gtag === 'function') {
+    window.gtag('config', 'G-LLNKZEJCBP', {
+      page_path: url,
+    });
+  }
+};
+
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPageView(location.pathname + location.search);
+  }, [location]);
+
   return (
-    <Router>
-      <div className='overflow-x-hidden'>
-        <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/Tech-Elevate" element={<TechElevate />} /> 
-        </Routes>
-      </div>
-    </Router>
+    <div className="overflow-x-hidden">
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/Tech-Elevate" element={<TechElevate />} /> 
+      </Routes>
+    </div>
   );
 };
 
-export default App;
+// Wrapping App with Router to enable useLocation
+const AppWithRouter = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWithRouter;
