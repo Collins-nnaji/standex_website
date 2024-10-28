1f6e4007ad0313a96512e888fead2b431da5da85
Collins Nnaji <collinsnnaji1@gmail.com>
2024-10-28 00:58:38 +0000

    dedd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,21 +16,18 @@ export const MenuLinks = [
     name: "Tech Elevate",
     link: "/Tech-Elevate",
   },
-]
+];
+
 const Navbar = () => {
-  const [showMenu, setShowMenu] = React.useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
-      
       // Make the navbar visible when scrolling up or at the top
-      setVisible(
-        (prevScrollPos > currentScrollPos) || currentScrollPos < 10
-      );
-      
+      setVisible((prevScrollPos > currentScrollPos) || currentScrollPos < 10);
       setPrevScrollPos(currentScrollPos);
     };
 
@@ -43,61 +40,56 @@ const Navbar = () => {
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
-  }
+  };
+
   return (
     <>
-      <nav className={`fixed top-0 left-0 right-0 bg-white z-50 transition-transform duration-300 ${
-        visible ? 'translate-y-0' : '-translate-y-full'}`}
-      >
-        <div className='container py-3 md:py-2 ' >
-            <div className='flex justify-between items-center' > 
-              {/* Logo Section */}
-              <div>
+      <nav className={`fixed top-0 left-0 right-0 bg-white z-50 transition-transform duration-300 ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
+        <div className='container mx-auto py-3 md:py-2 px-4'>
+          <div className='flex justify-between items-center'>
+            {/* Logo Section */}
+            <div>
               <Link to="/">
                 <img src={Logo} alt="Logo" className='w-32' />
               </Link>
-              </div>
-              {/* Desktop NavLinks Section */}
-              <div className='hidden md:block'>
-                <ul className='flex items-center gap-8'>
-                  {MenuLinks.map(( { id, name, link }) => {
-                    return(
-                      <li key={id} className='cursor-pointer py-4'>
-                        <Link
-                        to={link}
-                        className='text-lg font-medium hover:text-primary py-2 
-                        hover:border-b-2 hover:border-primary transition-all 
-                        duration-300'
-                      >
-                        {name}
-                      </Link>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-              {/* Mobile View */}
-              <div className='flex items-center gap-4 md:hidden'>
-                {showMenu ? (
-                  <HiMenuAlt1
-                    className='text-2xl cursor-pointer'
-                    onClick={toggleMenu}
-                  />
-                ) : (
-                  <HiMenuAlt3
-                    className='text-2xl cursor-pointer'
-                    onClick={toggleMenu}
-                  />
-                )}
-              </div>
             </div>
+            {/* Desktop NavLinks Section */}
+            <div className='hidden md:block'>
+              <ul className='flex items-center gap-8'>
+                {MenuLinks.map(({ id, name, link }) => (
+                  <li key={id} className='cursor-pointer py-4'>
+                    <Link
+                      to={link}
+                      className='text-lg font-medium hover:text-primary py-2 hover:border-b-2 hover:border-primary transition-all duration-300'
+                    >
+                      {name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* Mobile View */}
+            <div className='flex items-center gap-4 md:hidden'>
+              {showMenu ? (
+                <HiMenuAlt1
+                  className='text-2xl cursor-pointer'
+                  onClick={toggleMenu}
+                />
+              ) : (
+                <HiMenuAlt3
+                  className='text-2xl cursor-pointer'
+                  onClick={toggleMenu}
+                />
+              )}
+            </div>
+          </div>
         </div>
         {/* Mobile NavLinks Section */}
         <ResponsiveMenu showMenu={showMenu} />
       </nav>
       <div className="h-16 md:h-14"></div>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
