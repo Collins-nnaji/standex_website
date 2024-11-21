073bfa8899a3543066b51e4697258d9161ce0523
Collins Nnaji <collinsnnaji1@gmail.com>
2024-11-21 14:40:55 +0000

    aaa

--- src/components/ResponsiveMenu.jsx
+++ src/components/ResponsiveMenu.jsx
@@ -1,33 +1,42 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { MenuLinks } from './Navbar';
 
 const ResponsiveMenu = ({ showMenu }) => {
+  const navigate = useNavigate();
+
+  const handleLinkClick = (link) => {
+    navigate(link, { replace: true });
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+      window.location.reload();
+    }, 0);
+  };
+
   return (
     <div
-      className={`${showMenu ? "left-0" : "-left-full"} 
-      fixed top-0  w-[75%] transition-all duration-300 
-      shadow-md bg-white z-50 flex flex-col justify-between pt-16 px-8 pb-4`}
+     className={`${showMenu? "left-0" : "left-[-100%]"}
+     fixed top-0  w-[75%] transition-all duration-300
+     shadow-md pt-16 px-8 bg-white z-50 flex flex-col justify-between pb-4 `}
     >
-      <div>
-        <ul className="space-y-4 text-base">
-          {MenuLinks.map(({ id, name, link }) => (
-            <li key={id}>
-              <Link
-                to={link}
-                className="text-lg font-medium hover:text-primary transition-all duration-300"
-              >
-                {name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+      <div className='card'>
+        <nav className=''>
+          <ul className='space-y-4 text-base'>
+            {MenuLinks.map(({ id, name, link }) => (
+              <li key={id}>
+                <span 
+                  onClick={() => handleLinkClick(link)} 
+                  className='mb-4 inline-block cursor-pointer'
+                >
+                  {name}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-      {/* Optional Footer Section */}
-      <div className="text-center text-sm text-gray-500 mt-4 hidden md:block">
-        <p>&copy; {new Date().getFullYear()} Standex Digital. All rights reserved.</p>
-      </div>
+      <div></div>
     </div>
   );
 };
